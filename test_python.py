f7d7ecd760c37ba36ba02ff6a0878ed483c20178
pbreddy22 <18485830+pbreddy22@users.noreply.github.com>
2024-07-30 18:11:12 -0500

    Update test_python.py
    update all

--- test_python.py
+++ test_python.py
@@ -1,16 +1,24 @@
+import os
+
 # Config section at the top of the Snakefile
 configfile: "config.yaml"
-
-
 
 # Access the input and output folder paths from the config
 INPUT_FLD = config["input_folder"]
 OUTPUT_FLD = config["output_folder"]
 
+# Function to get all input gz files
+def get_input_files():
+    return [f for f in os.listdir(INPUT_FLD) if f.endswith('.gz')]
+
+# Function to get all output files
+def get_output_files(wildcards):
+    return expand(f"{OUTPUT_FLD}/{{file}}", file=[f.replace('.gz', '') for f in get_input_files()])
+
 # Rule to process all .gz files in the input folder
 rule all:
     input:
-        expand(f"{OUTPUT_FLD}/{{file}}", file=[f.replace('.gz', '') for f in os.listdir(INPUT_FLD) if f.endswith('.gz')])
+        get_output_files
 
 # Rule to gunzip files
 rule gunzip_files:
@@ -21,6 +29,5 @@ rule gunzip_files:
     shell:
         """
         echo "Unzipping {input} to {output}"
-        gunzip -c {input} > {output}
+        gunzip -c {{input}} > {{output}}
         """
-
