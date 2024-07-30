1edb8a1bd639ab5a20db7cd75177f274e93dfb4a
pbreddy22 <18485830+pbreddy22@users.noreply.github.com>
2024-07-30 18:21:12 -0500

    Update test_python.py
    update

--- test_python.py
+++ test_python.py
@@ -31,3 +31,42 @@ rule gunzip_files:
         echo "Unzipping {input} to {output}"
         gunzip -c {{input}} > {{output}}
         """
+
+
+
+
+
+
+import os
+
+# Config section at the top of the Snakefile
+configfile: "config.yaml"
+
+# Access the input and output folder paths from the config
+INPUT_FLD = config["input_folder"]
+OUTPUT_FLD = config["output_folder"]
+
+# Function to get all input gz files
+def get_input_files():
+    return [f for f in os.listdir(INPUT_FLD) if f.endswith('.gz')]
+
+# Function to get all output files
+def get_output_files():
+    return [os.path.join(OUTPUT_FLD, f.replace('.gz', '')) for f in get_input_files()]
+
+# Rule to process all .gz files in the input folder
+rule all:
+    input:
+        get_output_files()
+
+# Rule to gunzip files
+rule gunzip_files:
+    input:
+        os.path.join(INPUT_FLD, "{file}.gz")
+    output:
+        os.path.join(OUTPUT_FLD, "{file}")
+    shell:
+        """
+        echo "Unzipping {input} to {output}"
+        gunzip -c {input} > {output}
+        """
